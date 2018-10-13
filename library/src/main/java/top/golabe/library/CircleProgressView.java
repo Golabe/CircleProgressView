920f9877264bf475f7e82cba346ffdb240c27246
yuequ <2643552374@qq.com>
2018-10-13 14:00:43 +0800

    一部分优化

--- library/src/main/java/top/golabe/library/CircleProgressView.java
+++ library/src/main/java/top/golabe/library/CircleProgressView.java
@@ -8,7 +8,6 @@ import android.graphics.Paint;
 import android.graphics.Point;
 import android.graphics.RectF;
 import android.util.AttributeSet;
-import android.util.TypedValue;
 import android.view.View;
 
 public class CircleProgressView extends View {
@@ -21,8 +20,6 @@ public class CircleProgressView extends View {
     private int mProgress;
     private float mProgressTextSize;
     private float mProgressBarWidth;
-
-
     private Paint mPaintBg;
     private Paint mPaintText;
     private Paint mPaintProgressBar;
@@ -30,50 +27,51 @@ public class CircleProgressView extends View {
     private int mHeight;
     private int mRadius;
     private Point mCircleCenterPoint;
-    private RectF mRectF=new RectF();
+    private RectF mRectF = new RectF();
     private int mTotalProgress;
 
     public CircleProgressView(Context context) {
-        this(context,null);
+        this(context, null);
     }
 
-    public CircleProgressView(Context context,  AttributeSet attrs) {
-        this(context, attrs,0);
+    public CircleProgressView(Context context, AttributeSet attrs) {
+        this(context, attrs, 0);
     }
 
     public CircleProgressView(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
-        this.mContext=context;
+        this.mContext = context;
         initAttrs(attrs);
         init();
     }
 
     private void initAttrs(AttributeSet attrs) {
-        if (attrs!=null){
+        if (attrs != null) {
             TypedArray a = mContext.obtainStyledAttributes(attrs, R.styleable.CircleProgressView);
-            mProgress=a.getInt(R.styleable.CircleProgressView_progress,0);
-            mProgressMax=a.getInt(R.styleable.CircleProgressView_progress_max,100);
-            mProgressMin=a.getInt(R.styleable.CircleProgressView_progress_min,0);
-            mProgressTextSize=sp2px(a.getDimension(R.styleable.CircleProgressView_progress_text_size,14));
-            mProgressBarWidth=dp2px(a.getDimension(R.styleable.CircleProgressView_progress_bar_width,2));
-            mProgressBarColor=a.getColor(R.styleable.CircleProgressView_progress_bar_color,Color.WHITE);
-            mProgressBgColor=a.getColor(R.styleable.CircleProgressView_progress_bg_color,Color.BLUE);
-            mProgressTextColor=a.getColor(R.styleable.CircleProgressView_progress_text_color,Color.WHITE);
+            mProgress = a.getInt(R.styleable.CircleProgressView_progress, 0);
+            mProgressMax = a.getInt(R.styleable.CircleProgressView_progress_max, 100);
+            mProgressMin = a.getInt(R.styleable.CircleProgressView_progress_min, 0);
+            mProgressTextSize =a.getDimension(R.styleable.CircleProgressView_progress_text_size, 16);
+            mProgressBarWidth = dp2px(a.getDimension(R.styleable.CircleProgressView_progress_bar_width, 2));
+            mProgressBarColor = a.getColor(R.styleable.CircleProgressView_progress_bar_color, Color.WHITE);
+            mProgressBgColor = a.getColor(R.styleable.CircleProgressView_progress_bg_color, Color.BLUE);
+            mProgressTextColor = a.getColor(R.styleable.CircleProgressView_progress_text_color, Color.WHITE);
             checkProgress(mProgress);
             a.recycle();
         }
 
     }
+
     private void init() {
-        mPaintBg=new Paint(Paint.ANTI_ALIAS_FLAG);
+        mPaintBg = new Paint(Paint.ANTI_ALIAS_FLAG);
         mPaintBg.setColor(mProgressBgColor);
         mPaintBg.setStyle(Paint.Style.FILL);
 
-        mPaintText=new Paint(Paint.ANTI_ALIAS_FLAG);
+        mPaintText = new Paint(Paint.ANTI_ALIAS_FLAG);
         mPaintText.setColor(mProgressTextColor);
-        mPaintText.setTextSize(mProgressTextSize);
+        mPaintText.setTextSize(sp2px(mProgressTextSize));
 
-        mPaintProgressBar=new Paint(Paint.ANTI_ALIAS_FLAG);
+        mPaintProgressBar = new Paint(Paint.ANTI_ALIAS_FLAG);
         mPaintProgressBar.setColor(mProgressBarColor);
         mPaintProgressBar.setStyle(Paint.Style.STROKE);
         mPaintProgressBar.setStrokeCap(Paint.Cap.ROUND);
@@ -91,10 +89,10 @@ public class CircleProgressView extends View {
         int width = MeasureSpec.getSize(widthMeasureSpec);
         int height = MeasureSpec.getSize(heightMeasureSpec);
         int size = Math.min(width, height);
-        if (widthModel==MeasureSpec.EXACTLY||heightModel==MeasureSpec.EXACTLY){
-            setMeasuredDimension(size,size);
-        }else if (widthModel==MeasureSpec.AT_MOST||heightModel==MeasureSpec.AT_MOST){
-            setMeasuredDimension(dp2px(48),dp2px(48));
+        if (widthModel == MeasureSpec.EXACTLY || heightModel == MeasureSpec.EXACTLY) {
+            setMeasuredDimension(size, size);
+        } else if (widthModel == MeasureSpec.AT_MOST || heightModel == MeasureSpec.AT_MOST) {
+            setMeasuredDimension(dp2px(48), dp2px(48));
         }
 
     }
@@ -105,9 +103,9 @@ public class CircleProgressView extends View {
         super.onSizeChanged(w, h, oldw, oldh);
         mWidth = w;
         mHeight = h;
-        mRadius = w/2-w/10;
+        mRadius = w / 2 - w/20;
         mCircleCenterPoint = new Point(mWidth / 2, mHeight / 2);
-        mRectF.set(mWidth/8,mHeight/8,mWidth-mWidth/8,mHeight-mHeight/8);
+        mRectF.set(w/20*2,w/20*2, mWidth - w/20*2, mHeight - w/20*2);
     }
 
     @Override
@@ -116,22 +114,22 @@ public class CircleProgressView extends View {
         drawBg(canvas);
         drawText(canvas);
         canvas.save();
-        canvas.rotate(-90,mCircleCenterPoint.x,mCircleCenterPoint.y);
+        canvas.rotate(-90, mCircleCenterPoint.x, mCircleCenterPoint.y);
         drawProgressBar(canvas);
 
     }
 
     private void drawProgressBar(Canvas canvas) {
 
-        canvas.drawArc(mRectF,0, (float) ((360.0/mTotalProgress)*mProgress),false,mPaintProgressBar);
+        canvas.drawArc(mRectF, 0, (float) ((360.0 / mTotalProgress) * mProgress), false, mPaintProgressBar);
     }
 
     private void drawText(Canvas canvas) {
-        String progress=mProgress+"%";
-        float x = getWidth() / 2 - mPaintText.measureText(progress)/2;
+        String progress = mProgress + "%";
+        float x = getWidth() / 2 - mPaintText.measureText(progress) / 2;
         Paint.FontMetrics fm = mPaintText.getFontMetrics();
         float y = getHeight() / 2 - fm.descent + (fm.bottom - fm.top) / 2;
-        canvas.drawText(progress,x,y,mPaintText);
+        canvas.drawText(progress, x, y, mPaintText);
     }
 
     public int getProgressMax() {
@@ -184,16 +182,16 @@ public class CircleProgressView extends View {
     }
 
     public void setProgress(int progress) {
-      checkProgress(progress);
+        checkProgress(progress);
         invalidate();
     }
 
     private void checkProgress(int progress) {
-        if (progress<0){
-            this.mProgress=0;
-        }else if (progress>100){
-            this.mProgress=100;
-        }else {
+        if (progress < 0) {
+            this.mProgress = 0;
+        } else if (progress > 100) {
+            this.mProgress = 100;
+        } else {
             this.mProgress = progress;
         }
     }
@@ -218,16 +216,17 @@ public class CircleProgressView extends View {
 
 
     private void drawBg(Canvas canvas) {
-        canvas.drawCircle(mCircleCenterPoint.x,mCircleCenterPoint.y,mRadius,mPaintBg);
+        canvas.drawCircle(mCircleCenterPoint.x, mCircleCenterPoint.y, mRadius, mPaintBg);
     }
 
-    private   int sp2px( float spVal) {
-        return (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_SP,
-                spVal, mContext.getResources().getDisplayMetrics());
+    private int sp2px(float spValue) {
+        final float fontScale = mContext.getResources().getDisplayMetrics().scaledDensity;
+        return (int) (spValue * fontScale + 0.5f);
     }
-    private   int dp2px(float dpVal) {
-        return (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP,
-                dpVal, mContext.getResources().getDisplayMetrics());
+
+    private int dp2px(float dpValue) {
+        final float scale = mContext.getResources().getDisplayMetrics().density;
+        return (int) (dpValue * scale + 0.5f);
     }
 
 }
